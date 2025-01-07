4f303f4727c97738e1bfc615d6696cd60db87e8e
aidadieudonne <aidaharing10@gmail.com>
2025-01-06 22:47:57 -0700

    done

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] + array[j] === target) {
+        return true;
+      }
+    }
+  }
+  return false
 }
 
 /* 
   Write the Big O time complexity of your function here
+  the outer loop would have to iterate over the array n amount of times 
+  the inner loop would hav to iterate over the array n /2 amount of times, and it decreases as the outer loop increases
+  o(n^2)
 */
 
 /* 
   Add your pseudocode here
+  for let loop for 
+  for let loop for j 
+  if else method for result
 */
 
 /*
   Add written explanation of your solution here
+  the function needs to iterate over an array,
+  find a pair of numbers that add up to the target,
+  return true if two of any pair of numbers add up to the target
 */
 
 // You can run `node index.js` to view these console logs
